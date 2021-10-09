07651d7df836e26fcced210589b36f9830e3d9ca
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-10-09 17:40:08 -0300

    Mostrar grupos del usuario en el panel de admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,16 @@
-exports.panelAdministracion=(req,res)=>{
+const Grupos = require('../models/Grupos')
+
+exports.panelAdministracion=async(req,res)=>{
+ 
+
+    const grupos = await Grupos.findAll({
+        where: {
+            usuarioId: req.user.id
+        }
+      })
+    
     res.render('administracion',{
-        nombrePagina:'Panel de administracion' 
+        nombrePagina:'Panel de administracion',
+        grupos 
     })
 }
