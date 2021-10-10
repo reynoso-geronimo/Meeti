f38655473642e2b1997f60f41e7388ac44678c31
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-10-10 12:08:03 -0300

    Eliminar grupos

--- controllers/gruposController.js
+++ controllers/gruposController.js
@@ -1,125 +1,200 @@
-const Categorias = require('../models/Categorias')
-const Grupos = require('../models/grupos')
+const Categorias = require("../models/Categorias");
+const Grupos = require("../models/grupos");
 const { body } = require("express-validator");
-const multer= require('multer')
-const shortid= require('shortid')
+const multer = require("multer");
+const shortid = require("shortid");
+const fs= require('fs')
 
-const configuracionMulter= {
-    limits:{ fileSize : 100000 },
-    storage: fileStorage = multer.diskStorage({
-        destination:(req,file,next)=>{
-            next(null,__dirname+'/../public/uploads/grupos/');
-        },
-        filename:(req,file,next)=>{
-            const extension = file.mimetype.split('/')[1];
-            next(null, `${shortid.generate()}.${extension}`)
-        }
-    }),
-    fileFilter(req, file , next){
-        if(file.mimetype==='image/jpg' || file.mimetype==='image/png'){
-            //el formato es valido
-            next(null, true)
-        }else{
-            next(new Error('Formato de archivo no valido'), false)
-        }
+const configuracionMulter = {
+  limits: { fileSize: 100000 },
+  storage: (fileStorage = multer.diskStorage({
+    destination: (req, file, next) => {
+      next(null, __dirname + "/../public/uploads/grupos/");
+    },
+    filename: (req, file, next) => {
+      const extension = file.mimetype.split("/")[1];
+      next(null, `${shortid.generate()}.${extension}`);
+    },
+  })),
+  fileFilter(req, file, next) {
+    if (
+      file.mimetype === "image/jpg" ||
+      "image/jpeg" ||
+      file.mimetype === "image/png"
+    ) {
+      //el formato es valido
+      next(null, true);
+    } else {
+      next(new Error("Formato de archivo no valido"), false);
     }
-}
+  },
+};
 
-const upload= multer(configuracionMulter).single('imagen')
+const upload = multer(configuracionMulter).single("imagen");
 
-exports.subirImagen=(req,res,next)=>{
-    upload(req,res,function(error){
-        if(error){
-            if(error instanceof multer.MulterError){
-                if(error.code==='LIMIT_FILE_SIZE'){
-                    req.flash('error','El Archivo es muy grande limite 1 MB')
-                }else{
-                    req.flash('error', error.message)
-                }
-            }else if(error.hasOwnProperty('message')){
-                req.flash('error', error.message)
-            
-            }
-            res.redirect('back')
-            return
-            
-        }else{
-            next();
+exports.subirImagen = (req, res, next) => {
+  upload(req, res, function (error) {
+    if (error) {
+      if (error instanceof multer.MulterError) {
+        if (error.code === "LIMIT_FILE_SIZE") {
+          req.flash("error", "El Archivo es muy grande limite 1 MB");
+        } else {
+          req.flash("error", error.message);
         }
-    })
-}
+      } else if (error.hasOwnProperty("message")) {
+        req.flash("error", error.message);
+      }
+      res.redirect("back");
+      return;
+    } else {
+      next();
+    }
+  });
+};
 
-exports.formNuevoGrupo=async(req,res)=>{
-    const categorias = await Categorias.findAll();
-    
-    res.render('nuevo-grupo',{
-        nombrePagina:'Crea un Nuevo grupo',
-        categorias
-    })
+exports.formNuevoGrupo = async (req, res) => {
+  const categorias = await Categorias.findAll();
 
-}
-exports.crearGrupo= async(req,res,next)=>{
-    
-    const grupo = req.body
-    
-        body("nombre").escape(),
-        body("url").escape(),
-        body("descripcion").escape();
-      
-      grupo.usuarioId= req.user.id
+  res.render("nuevo-grupo", {
+    nombrePagina: "Crea un Nuevo grupo",
+    categorias,
+  });
+};
+exports.crearGrupo = async (req, res, next) => {
+  const grupo = req.body;
 
-      //leer la imagen
-      if(req.file){grupo.imagen= req.file.filename;}
-      
+  body("nombre").escape(), body("url").escape(), body("descripcion").escape();
 
+  grupo.usuarioId = req.user.id;
 
-   try {
-    await Grupos.create(grupo)
-    req.flash('exito', 'Grupo Creado Corretcamente')
-    res.redirect('/administracion')
-   } catch (error) {
+  //leer la imagen
+  if (req.file) {
+    grupo.imagen = req.file.filename;
+  }
+
+  try {
+    await Grupos.create(grupo);
+    req.flash("exito", "Grupo Creado Corretcamente");
+    res.redirect("/administracion");
+  } catch (error) {
     const erroresSequelize = Object.values(error.errors).map(
-        (err) => err.message
-      );
-        req.flash('error', erroresSequelize)
-       res.redirect('/nuevo-grupo')
-   }
+      (err) => err.message
+    );
+    req.flash("error", erroresSequelize);
+    res.redirect("/nuevo-grupo");
+  }
+};
+exports.formEditarGrupo = async (req, res) => {
+  const consultas = [];
+  consultas.push(Grupos.findByPk(req.params.grupoId));
+  consultas.push(Categorias.findAll());
+
+  //promise con await
+  const [grupo, categorias] = await Promise.all(consultas);
+
+  res.render("editar-grupo", {
+    nombrePagina: `Editar Grupo : ${grupo.nombre}`,
+    grupo,
+    categorias,
+  });
+};
+
+exports.EditarGrupo = async (req, res) => {
+  const grupo = await Grupos.findOne({where: { id: req.params.grupoId, usuarioId: req.user.id },});
+
+  if (!grupo) {
+    req.flash("error", "operacion no valida");
+    res.redirect("/administracion");
+    return next();
+  }
+  const { nombre, descripcion, categoriaId, url } = req.body;
+
+  //asignar los valores
+  grupo.nombre = nombre;
+  grupo.descripcion = descripcion;
+  grupo.categoriaId = categoriaId;
+  grupo.url = url;
+
+  //guardar en la base de datos
+  await grupo.save();
+  req.flash("exito", "Cambios almacenados exitosamente");
+  res.redirect("/administracion");
+};
+exports.formEditarImagen = async (req, res) => {
+    const grupo = await Grupos.findOne({where: { id: req.params.grupoId, usuarioId: req.user.id },});
+
+  res.render("imagen-grupo", {
+    nombrePagina: `Editar Imagen Grupo : ${grupo.nombre}`,
+    grupo,
+  });
+};
+exports.editarImagen = async (req, res, next) => {
+    const grupo = await Grupos.findOne({where: { id: req.params.grupoId, usuarioId: req.user.id },});
+  
+    if (!grupo) {
+    req.flash("error", "operacion no valida");
+    res.redirect("/administracion");
+    return next();
+  }
+
+  //si hay imagen anterior y nueva significa que vamos a borrar la anterior
+  if(req.file && grupo.imagen){
+      const imagenAnteriorPath= __dirname+`/../public/uploads/grupos/${grupo.imagen}`
+      
+      //eliminar archivo con fs
+      fs.unlink(imagenAnteriorPath,(error)=>{
+          if(error){
+              console.log(error)
+          }
+          return;
+      })
+  }
+  //si hay imagen nueva almacenar
+  if(req.file){
+      grupo.imagen = req.file.filename;
+  }
+  //guardar en la base de datos
+  
+  await grupo.save()
+
+  req.flash('exito', 'Cambios Almacenados correctamente')
+  res.redirect('/administracion')
+};
+exports.formEliminarGrupo= async (req,res,next)=>{
+  const grupo = await Grupos.findOne({where: { id: req.params.grupoId, usuarioId: req.user.id },});
+ if(!grupo){
+   req.flash('error','Operacion no valida'),
+   res.redirect('/administracion')
+   return next()
+ }
+ res.render('eliminar-grupo',{
+   nombrePagina:`Eliminar Grupo : ${grupo.nombre}`
+ })
 }
-exports.formEditarGrupo=async(req,res)=>{
-    
-
-    const consultas= [];
-    consultas.push(Grupos.findByPk(req.params.grupoId));
-    consultas.push(Categorias.findAll() )
-
-    //promise con await
-    const [grupo,categorias ] = await Promise.all(consultas)
-
-    res.render('editar-grupo',{
-        nombrePagina: `Editar Grupo : ${grupo.nombre}`,
-        grupo,
-        categorias
-    })
-}
-
-exports.EditarGrupo=async(req,res)=>{
-    const grupo = await Grupos.findOne({where:{id:req.params.grupoId, usuarioId: req.user.id}})
-
-    if(!grupo){
-        req.flash('error','operacion no valida');
-        res.redirect('/administracion')
-        return next()
-    }
-    const {nombre, descripcion, categoriaId , url} = req.body
-
-    //asignar los valores
-    grupo.nombre= nombre;
-    grupo.descripcion=descripcion;
-    grupo.categoriaId=categoriaId;
-    grupo.url=url;
-
-    //guardar en la base de datos
-    await grupo.save();
-    req.flash('exito', 'Cambios almacenados exitosamente');
+exports.eliminarGrupo=async(req,res,next)=>{
+  const grupo = await Grupos.findOne({where: { id: req.params.grupoId, usuarioId: req.user.id },});
+  if(!grupo){
+    req.flash('error','Operacion no valida'),
     res.redirect('/administracion')
+    return next()
+  }
+  console.log(grupo.imagen)
+  if(grupo.imagen){
+    const imagen= __dirname+`/../public/uploads/grupos/${grupo.imagen}`
+      
+      //eliminar archivo con fs
+      fs.unlink(imagen,(error)=>{
+          if(error){
+              console.log(error)
+          }
+          return;
+      })
+  }
+  await Grupos.destroy({
+    where:{
+      id:req.params.grupoId
+    }
+  })
+req.flash('exito', 'Grupo eliminado');
+res.redirect('/administracion')
 }
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,75 @@
-const express = require('express')
-const router= express.Router()
-const homeController= require('../controllers/homeController')
-const usuariosController= require('../controllers/usuariosController')
-const authController= require('../controllers/authController')
-const adminController= require('../controllers/adminController')
-const gruposController=require('../controllers/gruposController')
+const express = require("express");
+const router = express.Router();
+const homeController = require("../controllers/homeController");
+const usuariosController = require("../controllers/usuariosController");
+const authController = require("../controllers/authController");
+const adminController = require("../controllers/adminController");
+const gruposController = require("../controllers/gruposController");
+const meetiController = require("../controllers/meetiController");
 
-module.exports = function (){
-    router.get('/', homeController.home)
-    router.get('/crear-cuenta', usuariosController.formCrearCuenta);
-    router.post('/crear-cuenta', usuariosController.crearNuevoUsuario)
-    router.get('/confirmar-cuenta/:correo', usuariosController.confirmarCuenta)
+module.exports = function () {
+  router.get("/", homeController.home);
+  router.get("/crear-cuenta", usuariosController.formCrearCuenta);
+  router.post("/crear-cuenta", usuariosController.crearNuevoUsuario);
+  router.get("/confirmar-cuenta/:correo", usuariosController.confirmarCuenta);
 
-    router.get('/iniciar-sesion', usuariosController.formIniciarSesion)
-    router.post('/iniciar-sesion', authController.autenticarUsuario)
-    
-    //panel de admin
-    router.get('/administracion',authController.usuarioAutenticado, adminController.panelAdministracion)
-    router.get('/nuevo-grupo',authController.usuarioAutenticado, gruposController.formNuevoGrupo )
-    router.post('/nuevo-grupo',authController.usuarioAutenticado, gruposController.subirImagen,gruposController.crearGrupo )
-    router.get('/editar-grupo/:grupoId',
-    authController.usuarioAutenticado,gruposController.formEditarGrupo
-    )
-    router.post('/editar-grupo/:grupoId',
-    authController.usuarioAutenticado,gruposController.EditarGrupo
-    )
+  router.get("/iniciar-sesion", usuariosController.formIniciarSesion);
+  router.post("/iniciar-sesion", authController.autenticarUsuario);
 
-    return router
-}
+  //panel de admin
+  router.get(
+    "/administracion",
+    authController.usuarioAutenticado,
+    adminController.panelAdministracion
+  );
+  router.get(
+    "/nuevo-grupo",
+    authController.usuarioAutenticado,
+    gruposController.formNuevoGrupo
+  );
+  router.post(
+    "/nuevo-grupo",
+    authController.usuarioAutenticado,
+    gruposController.subirImagen,
+    gruposController.crearGrupo
+  );
+  router.get(
+    "/editar-grupo/:grupoId",
+    authController.usuarioAutenticado,
+    gruposController.formEditarGrupo
+  );
+  router.post(
+    "/editar-grupo/:grupoId",
+    authController.usuarioAutenticado,
+    gruposController.EditarGrupo
+  );
+  //editar la imagen del grupo
+  router.get(
+    "/imagen-grupo/:grupoId",
+    authController.usuarioAutenticado,
+    gruposController.formEditarImagen
+  );
+  router.post(
+    "/imagen-grupo/:grupoId",
+    authController.usuarioAutenticado,
+    gruposController.subirImagen,
+    gruposController.editarImagen
+  );
+  //elimar grupos
+  router.get(
+    "/eliminar-grupo/:grupoId",
+    authController.usuarioAutenticado,
+    gruposController.formEliminarGrupo
+  );
+  router.post(
+    "/eliminar-grupo/:grupoId",
+    authController.usuarioAutenticado,
+    gruposController.eliminarGrupo
+  );
+  router.get(
+    "/nuevo-meeti",
+    authController.usuarioAutenticado,
+    meetiController.formNuevoMeeti
+  );
+  return router;
+};
